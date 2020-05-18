5618aaac97dac1b21664b48a5b375637e125340a
Niall Maguire <maguiren@phonovation.com>
2020-05-18 22:33:30 +0100

    body setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/footer';
+import Body from './Components/Body/Body';
 
 function App() {
   return (
     <div className="App">
       <div>
       <Header/>
+      </div>
+      <div>
+      <Body/>
       </div>
       <div>
       <Footer/>
---
+++ src/Components/Body/Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Body.css";
+import "../../CSS/bootstrap.min.css";
+
+
+ 
+function Body(props) {
+  return (
+    <div>Hello</div>
+    );
+}
+
+export default Body
