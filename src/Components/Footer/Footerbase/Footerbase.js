312f2486559507d83744712897fd939dc62602de
Niall Maguire <maguiren@phonovation.com>
2020-05-19 21:57:26 +0100

    added font awesome

--- src/Components/Footer/Footerbase/Footerbase.js
+++ src/Components/Footer/Footerbase/Footerbase.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styles from "./Footerbase.module.css";
 import "../../../CSS/bootstrap.min.css";
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 function Footerbase(props) {
   return (
-    <div className={styles.footer}>TEST</div>
+    <React.Fragment>
+      <div >
+        <FontAwesomeIcon icon={faFacebook} size="2x" className={styles.icon} />
+        <FontAwesomeIcon icon={faTwitter} size="2x" className={styles.icon} />
+        <FontAwesomeIcon icon={faInstagram} size="2x" className={styles.icon} />
+      </div>
+    </React.Fragment>
   );
 }
 
