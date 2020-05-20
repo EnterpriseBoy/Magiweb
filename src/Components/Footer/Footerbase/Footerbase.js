1fd159c6b17ac6b9a73315ac28bd7743fe0a687b
Niall Maguire <maguiren@phonovation.com>
2020-05-20 23:43:16 +0100

    Styling bottom boxes

--- src/Components/Footer/Footerbase/Footerbase.js
+++ src/Components/Footer/Footerbase/Footerbase.js
@@ -3,15 +3,51 @@ import styles from "./Footerbase.module.css";
 import "../../../CSS/bootstrap.min.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faMap,faRuler,faMailBulk } from '@fortawesome/free-solid-svg-icons'
+
+import { Row, Col } from 'reactstrap';
 
 function Footerbase(props) {
   return (
     <React.Fragment>
-      <div >
-        <FontAwesomeIcon icon={faFacebook} size="2x" className={styles.icon} />
-        <FontAwesomeIcon icon={faTwitter} size="2x" className={styles.icon} />
-        <FontAwesomeIcon icon={faInstagram} size="2x" className={styles.icon} />
-      </div>
+
+      <container>
+        <Row>
+          <Col className={styles.boxed}>
+            <Row><FontAwesomeIcon icon={faMap} size=".5x" className={styles.iconCenterCommon}/></Row>
+            <Row><div className={styles.iconCenterCommon}>EMAIL</div></Row>
+            <Row><FontAwesomeIcon icon={faRuler} size=".5x" className={styles.iconCenterCommon}/></Row>
+            <Row><div className={styles.iconCenterCommon}>72 Patrician Villas Blackrock</div></Row>
+          </Col>
+          <Col className={styles.boxed}>
+            <Row><FontAwesomeIcon icon={faMap} size=".5x" className={styles.iconCenterCommon}/></Row>
+            <Row><div className={styles.iconCenterCommon}>EMAIL</div></Row>
+            <Row><FontAwesomeIcon icon={faRuler} size=".5x" className={styles.iconCenterCommon}/></Row>
+            <Row><div className={styles.iconCenterCommon}>72 Patrician Villas Blackrock</div></Row>
+          </Col>
+          <Col className={styles.boxed}>
+            <Row><FontAwesomeIcon icon={faMap} size=".5x" className={styles.iconCenterCommon}/></Row>
+            <Row><div className={styles.iconCenterCommon}>EMAIL</div></Row>
+            <Row><FontAwesomeIcon icon={faRuler} size=".5x" className={styles.iconCenterCommon}/></Row>
+            <Row><div className={styles.iconCenterCommon}>72 Patrician Villas Blackrock</div></Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col/>
+          <Col/>
+          <Col>
+            <FontAwesomeIcon icon={faFacebook} size="2x" className={styles.iconRight} />
+          </Col>
+          <Col>
+            <FontAwesomeIcon icon={faTwitter} size="2x" className={styles.iconCenter} />
+          </Col>
+          <Col>
+            <FontAwesomeIcon icon={faInstagram} size="2x" className={styles.iconLeft} />
+          </Col>
+          <Col/>
+          <Col/>
+        </Row>
+      </container>
     </React.Fragment>
   );
 }
