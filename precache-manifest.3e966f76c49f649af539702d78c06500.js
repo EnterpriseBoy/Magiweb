c684b366e6a610448d19c6fa09d20741cb08a117
Niall <niall@Nialls-MacBook-Pro.local>
2020-05-24 01:30:27 +0100

    Updates

---
+++ precache-manifest.3e966f76c49f649af539702d78c06500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9388d8161a4201d5bef1b072bee4037",
+    "url": "/https//github.com/EnterpriseBoy/Magiweb.git/index.html"
+  },
+  {
+    "revision": "05f613fbda789e7358df",
+    "url": "/https//github.com/EnterpriseBoy/Magiweb.git/static/css/main.eec34bc7.chunk.css"
+  },
+  {
+    "revision": "70c00dcd554b3c2e5777",
+    "url": "/https//github.com/EnterpriseBoy/Magiweb.git/static/js/2.2e29df63.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/https//github.com/EnterpriseBoy/Magiweb.git/static/js/2.2e29df63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f613fbda789e7358df",
+    "url": "/https//github.com/EnterpriseBoy/Magiweb.git/static/js/main.aa63a565.chunk.js"
+  },
+  {
+    "revision": "292b397a09de4dadc38c",
+    "url": "/https//github.com/EnterpriseBoy/Magiweb.git/static/js/runtime-main.c90993f9.js"
+  },
+  {
+    "revision": "429ed557b3e80530214d25336e343f4a",
+    "url": "/https//github.com/EnterpriseBoy/Magiweb.git/static/media/Background.429ed557.jpg"
+  }
+]);
