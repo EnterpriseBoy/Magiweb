8410fdf846f0350ba672ac16faefef2c315eb360
Niall Maguire <maguiren@phonovation.com>
2020-05-28 22:37:12 +0100

    partially working buttons

--- src/Components/Header/menubar/Menubar.js
+++ src/Components/Header/menubar/Menubar.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Menubar.css";
 import "../../../CSS/bootstrap.min.css";
 
 
 
 function Menubase(props) {
+
+  const[selectedNavItem,setSelectedNavItem] = useState("nav-link js-scroll-trigger");
+
+  function itemSelected(item){
+    item==="nav-link js-scroll-trigger nav-item-selected"? setSelectedNavItem("nav-link js-scroll-trigger nav-item-selected") :setSelectedNavItem("nav-link js-scroll-trigger nav-item-selected")
+    alert(item);
+  }
+
   return (
     <React.Fragment>
       <nav
@@ -17,17 +25,17 @@ function Menubase(props) {
         </a>
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#about">
+              <a className={selectedNavItem} href="#about" id="about1" onClick={e => itemSelected(e.target.className)}>
                 About
             </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#projects">
+              <a className={selectedNavItem} href="#projects">
                 Projects
             </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link js-scroll-trigger" href="#signup">
+              <a className={selectedNavItem} href="#signup">
                 Contact
             </a>
             </li>
