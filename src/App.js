883b669e67ecc937b0b213f72c1be03e3e53b8ac
Niall Maguire <maguiren@phonovation.com>
2020-06-01 21:22:20 +0100

    routing setup

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/footer';
 import Body from './Components/Body/Body';
+import About from './Components/Pages/About/About';
+import Contact from './Components/Pages/Contact/contact';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Components/Pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <div className="header">
-      <Header/>
-      </div>
-      <div>
-      <Body/>
-      </div>
-      <div className="footer">
-      <Footer/>
-      </div>
-    </div>
+    <Router>
+        <div className="App">
+          <div className="header">
+            <Header />
+          </div>
+          <div className="body">
+            <Switch>
+              <Route path="/About" component={About} />
+              <Route path="/Contact" component={Contact} />
+              <Route path="/Login" component={Login} />
+            </Switch>
+          </div>
+          <div className="footer">
+            <Footer />
+          </div>
+        </div>
+    </Router>
   );
 }
 
--- src/Components/Header/menubar/Menubar.js
+++ src/Components/Header/menubar/Menubar.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import "./Menubar.css";
 import "../../../CSS/bootstrap.min.css";
+import {Link} from "react-router-dom";
 
 
 
@@ -23,23 +24,23 @@ function Menubase(props) {
         <div className="container-fluid">
           <a className="navbar-brand" href="#page-top">
             Event Staff
-        </a>
+          </a>
           <ul className="navbar-nav ml-auto">
+            <Link to='/about'>
             <li className={selectButton === "about"? selecteButton: notSelectedButton }>
-              <a className="nav-link js-scroll-trigger" href="#about" id="about" onClick={e => itemSelected(e.target.id)}>
-                About
-            </a>
+              <a className="nav-link js-scroll-trigger" href="#about" id="about" onClick={e => itemSelected(e.target.id)}>About</a>
             </li>
+            </Link>
+            <Link to='contact'>
             <li className={selectButton === "projects"? selecteButton: notSelectedButton }>
-              <a className="nav-link js-scroll-trigger" id="projects" href="#projects" onClick={e => itemSelected(e.target.id)}>
-                Projects
-            </a>
+              <a className="nav-link js-scroll-trigger" id="projects" href="#projects" onClick={e => itemSelected(e.target.id)}>Projects</a>
             </li>
+            </Link>
+            <Link to='login'>
             <li className={selectButton === "login"? selecteButton: notSelectedButton }>
-              <a className="nav-link js-scroll-trigger" id="login" href="#signup" onClick={e => itemSelected(e.target.id)}>
-                Login
-            </a>
+              <a className="nav-link js-scroll-trigger" id="login" href="#signup" onClick={e => itemSelected(e.target.id)}>Login</a>
             </li>
+            </Link>
           </ul>
         </div>
       </nav>
