479bdc5bb2d68106042f7686cedd2902264e20bd
Niall <niall@MacBook-Pro.local>
2020-10-22 23:17:34 +0100

    connection working

--- src/Components/Modals/loginModal.js
+++ src/Components/Modals/loginModal.js
@@ -1,11 +1,27 @@
-import React, { useState,useRef,useEffect } from "react";
+import React, { useState,useRef } from "react";
 import { MDBContainer,MDBModalFooter, MDBBtn, MDBModal } from 'mdbreact';
+import axios from 'axios';
+import {useQuery} from 'react-query';
 import SliderSwitch from "../Helpers/SlliderSwitch/SliderSwitch";
 
 const LoginModal = (props) => {
   const [isChecked,setIsChecked] = useState(false);
+  const [username,setUsername] = useState("niall.maguire@topmail.ie");
+  const [password,setPassword] = useState("nijjjjjj");
+
   const nameDiv = useRef(null);
   const repeatPwd = useRef(null);
+
+  // const { isLoading, error, data } = useQuery('fetchLuke', () =>
+  // axios.post('http://localhost:3000/api/user/login',{
+  //   headers: {
+  //     'Content-Type': 'application/json ',
+  //     'accept': '*/*'
+  //   },
+  //     email: "niall.maguire@topmail.ie",
+  //     password: "nijjjjjj"
+  //   })
+  // );
 
   
   function onChange(arg){
@@ -21,7 +37,15 @@ const LoginModal = (props) => {
   }
 
   function handleSubmit(){
-    alert("TEST");
+    axios.post('http://localhost:3000/api/user/login',{
+        email: "niall.maguire@topmail.ie",
+        password: "nijjjjjj"
+      }).then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
 return (
@@ -36,10 +60,10 @@ return (
               <input type="text" className="form-control login_lable hide_mouse_over" placeholder="Name" aria-label="Name" aria-describedby="basic-addon" />
             </div>
             <div className="flex-grid">
-              <input type="text" className="form-control login_lable" placeholder="Username" aria-label="Username" aria-describedby="basic-addon" />
+              <input type="text" onChange={(e) => setUsername(e.target.value)} className="form-control login_lable" placeholder="Username" aria-label="Username" aria-describedby="basic-addon" />
             </div>
             <div className="flex-grid">
-              <input type="text" className="form-control login_lable" placeholder="Password" aria-label="Password" aria-describedby="basic-addon" />
+              <input type="text" onChange={(e) => setPassword(e.target.value)} className="form-control login_lable" placeholder="Password" aria-label="Password" aria-describedby="basic-addon" />
             </div>
             <div className="flex-grid" ref={repeatPwd} style = {{display: "none"}} >
               <input type="text" className="form-control login_lable" placeholder="Repeat Password"  aria-label="Repeat Password" aria-describedby="basic-addon" />
@@ -49,6 +73,14 @@ return (
           <MDBBtn color="outline secondary" onClick={props.handleClose} >Cancle</MDBBtn>
           </MDBModalFooter>
         </MDBModal>
+{/* 
+        {isLoading ? (
+        <div>Retrieving Luke Skywalker Information ...</div>
+      ) : (
+        <pre>{JSON.stringify(data, null, 2)}
+        </pre>
+      )} */}
+
       </MDBContainer>
 );
 };
