2d4bca16e1b4f152f5ac709b4cde4a205fe5689e
Niall <niall@MacBook-Pro.local>
2020-10-23 23:17:31 +0100

    animation working on login button

--- src/Components/Modals/loginModal.js
+++ src/Components/Modals/loginModal.js
@@ -1,47 +1,36 @@
 import React, { useState,useRef } from "react";
+import AnimateHeight from 'react-animate-height'
 import { MDBContainer,MDBModalFooter, MDBBtn, MDBModal } from 'mdbreact';
 import axios from 'axios';
-import {useQuery} from 'react-query';
 import SliderSwitch from "../Helpers/SlliderSwitch/SliderSwitch";
 
 const LoginModal = (props) => {
   const [isChecked,setIsChecked] = useState(false);
-  const [username,setUsername] = useState("niall.maguire@topmail.ie");
-  const [password,setPassword] = useState("nijjjjjj");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [height,setHeight]= useState("0");
 
   const nameDiv = useRef(null);
   const repeatPwd = useRef(null);
 
-  // const { isLoading, error, data } = useQuery('fetchLuke', () =>
-  // axios.post('http://localhost:3000/api/user/login',{
-  //   headers: {
-  //     'Content-Type': 'application/json ',
-  //     'accept': '*/*'
-  //   },
-  //     email: "niall.maguire@topmail.ie",
-  //     password: "nijjjjjj"
-  //   })
-  // );
-
-  
   function onChange(arg){
     if(isChecked){
-      nameDiv.current.style.display="none";
-      repeatPwd.current.style.display="none";
+      setHeight("0");
       setIsChecked(false);
     }else{
-      nameDiv.current.style.display="block";
-      repeatPwd.current.style.display="block";
+      setHeight("auto");
       setIsChecked(true);
     }
   }
 
   function handleSubmit(){
+    setHeight("äuto");
     axios.post('http://localhost:3000/api/user/login',{
-        email: "niall.maguire@topmail.ie",
-        password: "nijjjjjj"
+        email: email,
+        password: password
       }).then(function (response) {
-        console.log(response);
+        alert(JSON.stringify(response));
+        props.handleClose();
       })
       .catch(function (error) {
         console.log(error);
@@ -56,31 +45,27 @@ return (
               <div className="col"><SliderSwitch onChange={() => onChange } isChecked={isChecked}/></div>
               <div className="col login_lable">Register</div>
             </div>
-            <div className="flex-grid" ref={nameDiv} style = {{display: "none"}} >
-              <input type="text" className="form-control login_lable hide_mouse_over" placeholder="Name" aria-label="Name" aria-describedby="basic-addon" />
-            </div>
+            <AnimateHeight duration={ 500 } height={height}>
+              <div className="flex-grid" ref={nameDiv}  >
+                <input type="text" className="form-control login_lable" placeholder="Name" aria-label="Name" aria-describedby="basic-addon" />
+              </div>
+            </AnimateHeight>
             <div className="flex-grid">
-              <input type="text" onChange={(e) => setUsername(e.target.value)} className="form-control login_lable" placeholder="Username" aria-label="Username" aria-describedby="basic-addon" />
+              <input type="text" onChange={(e) => setEmail(e.target.value)} className="form-control login_lable" placeholder="Username" aria-label="Username" aria-describedby="basic-addon" />
             </div>
             <div className="flex-grid">
               <input type="text" onChange={(e) => setPassword(e.target.value)} className="form-control login_lable" placeholder="Password" aria-label="Password" aria-describedby="basic-addon" />
             </div>
-            <div className="flex-grid" ref={repeatPwd} style = {{display: "none"}} >
-              <input type="text" className="form-control login_lable" placeholder="Repeat Password"  aria-label="Repeat Password" aria-describedby="basic-addon" />
-            </div>
+            <AnimateHeight duration={ 500 } height={height}>
+              <div className="flex-grid" ref={repeatPwd}>
+                <input type="text" className="form-control login_lable" placeholder="Repeat Password"  aria-label="Repeat Password" aria-describedby="basic-addon" />
+              </div>
+            </AnimateHeight>
           <MDBModalFooter>
           <MDBBtn color="outline primary" onClick={handleSubmit}>Submit</MDBBtn>
           <MDBBtn color="outline secondary" onClick={props.handleClose} >Cancle</MDBBtn>
           </MDBModalFooter>
         </MDBModal>
-{/* 
-        {isLoading ? (
-        <div>Retrieving Luke Skywalker Information ...</div>
-      ) : (
-        <pre>{JSON.stringify(data, null, 2)}
-        </pre>
-      )} */}
-
       </MDBContainer>
 );
 };
