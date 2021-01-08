9ad8cd63ea71e52e2906461b0f220335711e048c
Niall <niall@MacBook-Pro.local>
2021-01-08 22:55:49 +0000

    form started

--- src/Components/Footer/Footerbase/Footerbase.js
+++ src/Components/Footer/Footerbase/Footerbase.js
@@ -17,11 +17,11 @@ function Footerbase(props) {
               <Row><div className={styles.iconCenterCommon}>info</div></Row>
             </Col>
             <Col className={styles.boxed}>
-  <Row><FontAwesomeIcon icon={faMap}  className={styles.iconCenterCommon} />{process.env.REACT_APP_ENVIORMENT}</Row>
+  <Row><FontAwesomeIcon icon={faMap}  className={styles.iconCenterCommon} /></Row>
               <Row><div className={styles.iconCenterCommon}>info</div></Row>
             </Col>
             <Col className={styles.boxed}>
-              <Row><FontAwesomeIcon icon={faMap} className={styles.iconCenterCommon} />{process.env.REACT_APP_LOGIN}</Row>
+              <Row><FontAwesomeIcon icon={faMap} className={styles.iconCenterCommon} /></Row>
               <Row><div className={styles.iconCenterCommon}>info</div></Row>
             </Col>
           </Row>
--- src/Components/Pages/Contact/contact.js
+++ src/Components/Pages/Contact/contact.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+
 
 
 function Contact() {
   return (
     <div className="App">
-      <h1>Contact</h1>
+      <div className="flex-container">
+        <div className="contactForm">
+          <div className="contactFormDetails">
+            <MDBContainer>
+              <MDBRow>
+                <MDBCol md="6">
+                  <form>
+                    <p className="h5 text-center mb-4">Sign in</p>
+                    <div className="grey-text">
+                      <MDBInput label="Type your email" icon="envelope" group type="email" validate error="wrong" success="right" />
+                      <MDBInput label="Type your password" icon="lock" group type="password" validate />
+                    </div>
+                    <div className="text-center">
+                      <MDBBtn>Login</MDBBtn>
+                    </div>
+                  </form>
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
+          </div>
+          <div className="contactFormMap">
+              <h1>Map</h1>
+          </div>
+        </div>
+      </div>
     </div>  
   );
 }
