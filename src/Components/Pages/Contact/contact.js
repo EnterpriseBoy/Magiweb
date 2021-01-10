3ac3e448a71902899969a2b37e472e2e0390cc66
Niall <niall@MacBook-Pro.local>
2021-01-10 20:39:57 +0000

    form layout improved

--- src/Components/Pages/Contact/contact.js
+++ src/Components/Pages/Contact/contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn,MDBIcon } from 'mdbreact';
+import ContactForm from './contactForm'
+import ContactMap from './contactMap'
 
 
 
@@ -8,31 +9,9 @@ function Contact() {
     <div className="App">
       <div className="flex-container">
         <div className="contactForm">
-          <div className="contactFormDetails">
-          <MDBContainer>
-            <MDBRow>
-              <MDBCol md="12">
-                <form>
-                  <p className="h5 text-center mb-4">Contact Us</p>
-                  <div className="grey-text">
-                    <MDBInput className="ContactInput" label="Name" group type="text" validate error="wrong" success="right" />
-                    <MDBInput className="ContactInput" label="Email" group type="email" validate error="wrong" success="right" />
-                    <MDBInput className="ContactInput" label="Subject" group type="text" validate error="wrong" success="right" />
-                    <MDBInput className="ContactInput" type="textarea" rows="2" label="Message" />
-                  </div>
-                  <div className="text-center">
-                    <MDBBtn outline color="secondary">
-                      Send
-                      <MDBIcon far icon="paper-plane" className="ml-1" />
-                    </MDBBtn>
-                  </div>
-                </form>
-              </MDBCol>
-            </MDBRow>
-          </MDBContainer>
-          </div>
+          <ContactForm/>
           <div className="contactFormMap">
-              <h1>Map</h1>
+              <ContactMap/>
           </div>
         </div>
       </div>
---
+++ src/Components/Pages/Contact/contactMap.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn,MDBIcon } from 'mdbreact';
+
+function ContacatMap() {
+  return (
+    <div className="contactFormDetails">
+      <MDBContainer>
+          <MDBRow>
+              <MDBCol md="12">
+              <form>
+                  <p className="h5 text-center mb-4">Contact Us</p>
+                  <div className="grey-text">
+                    <MDBInput className="ContactInput" label="Name" group type="text" validate error="wrong" success="right" />
+                    <MDBInput className="ContactInput" label="Email" group type="email" validate error="wrong" success="right" />
+                    <MDBInput className="ContactInput" label="Subject" group type="text" validate error="wrong" success="right" />
+                    <MDBInput className="ContactInput" type="textarea" rows="2" label="Message" />
+                  </div>
+                  <div className="text-center">
+                  <MDBBtn outline color="secondary">Send
+                      <MDBIcon far icon="paper-plane" className="ml-1" />
+                  </MDBBtn>
+                  </div>
+              </form>
+              </MDBCol>
+          </MDBRow>
+      </MDBContainer>
+    </div>
+  );
+}
+
+export default ContacatMap;
