d5a712ff8636294cdcf463c07f451caab312f6d5
Niall <niall@MacBook-Pro.local>
2021-01-20 00:12:50 +0000

    fixing home page spelling

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -8,11 +8,11 @@ function Home() {
         </div>
         <hr/>
         <div className="areaHeading">
-          <h4>Welcome to Event Management website, a site to organize and coordinate all your events</h4>
+          <h4>Welcome to Event Management website, a site to organize and coordinate all your events.</h4>
           <h4>The frontend is written in React and will communicate to an ASP.Net Core backend for</h4>
           <h4>access and storing event details. A node.js server will be used to track staff on site</h4>
-          <h4>with periodic updates to the ASP.net Server using firebase. An Android mobile will app</h4>
-          <h4>will be written to track and communicate with staff during the event.</h4>
+          <h4>with periodic updates to the ASP.net Server. An Android mobile will app will be written</h4>
+          <h4>to track and communicate with staff during the event and record details on site as needed.</h4>
         </div>
     </div>  
   );
